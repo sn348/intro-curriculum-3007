979aa6b33140c04e572278374da093d0ea4f4c70
sn348 <sn348@example.com>
2020-04-30 13:53:26 +0900

    17で割り切れるかどうかを判断する関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-module.exports = {
+//テストコードではリストaを関数isMultipleOfSeventeenでfilterしている
+//これにマッチするisMultipleOfSeventeenを定義すればよい
+
+function isMultipleOfSeventeen(num)
+{
+    return num % 17 === 0;
 }
+
+module.exports = {
+    isMultipleOfSeventeen
+};
